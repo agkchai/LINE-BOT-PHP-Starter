a320ab6fffe08797f2083250bdefb73e9f7241e2
agkchai <5615261042@rbru.ac.th>
2017-04-06 23:33:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,16 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				//'type' => 'text',
-				//'text' => $text
-				 {
-            "type":"text",
-            "text":"hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
+				'type' => 'text',
+				'text' => 
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
